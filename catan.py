1bd1398973dff3525b7b88abbc603e1031e47b20
Damien de Mijolla <damiendemijolla@gmail.com>
2018-12-29 18:52:52 +0000

    Included Skeletton For API

---
+++ catan.py
@@ -0,0 +1,75 @@
+class Game():
+    def __init__():
+        self.game_board = initialize_game_board()
+
+    def setup():
+        setup_menu() #Chose the number of players
+        intialize_board()
+
+    def initialize_board():
+        return something::GameBoard
+
+    def check_road_valid():
+        pass
+    def check_settlement_valid():
+        pass
+
+class GameBoard():
+    def __init__():
+        self.tiles = []
+        self.players= []
+    def create_tile(coords, N, ressource):
+        """coord: cordinates of tile
+        ressource: ressource type of tile"""
+        return Tile(coords,N,ressource)
+
+class Tile():
+    def __init__(self):
+        self.coords = coords
+        self.N=N
+        self.ressource=ressource
+
+    def get_neighboors(self):
+        self.N+1
+
+class Settlement():
+    def __init__():
+        pass
+    def 
+
+class Road():
+    def __init__():
+        self.coords=
+
+class Robber():
+    def __init__():
+        pass
+
+class DevelopmentCards():
+    def __init__():
+        pass
+
+
+class Player():
+    def __init__():
+        self.settlements = [] 
+        self.roads = [road1,road2]
+        self.brick
+        self.lumber
+        self.wool
+        self.ore
+        self.wheat
+        self.development_cards
+        self.is_turn= False
+    def create_road(coords):
+        self.lumber -=1
+        self.wood = -=1
+        roads.append = Road(coords)
+        pass
+    def get_development_card():
+        development_cards.append(DevelopmentCards())
+
+    
+    def create_settlement(coords):
+        pass
+
