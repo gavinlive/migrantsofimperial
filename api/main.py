10e605fedc8b906b4b9d68274883a95b64b25fd0
Gavin S <gavin@gavinseegoolam.co.uk>
2018-12-29 19:04:46 +0000

    Init Flask Dev. Commit

--- api/main.py
+++ api/main.py
@@ -1,1 +1,30 @@
 ## Using the Flask microframework for Python
+## Adopt a MVC (Model-View-Controller) Design
+
+
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+from src.models.game import load_game
+
+# initialize instance of WSGI application
+# act as a central registry for the view functions, URL rules, template configs
+app = Flask(__name__)
+
+## include db name in URI; _HOST entry overwrites all others
+app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/settlersofcatan'
+app.debug = True
+mongo = PyMongo(app)
+
+# global vars: app, mongo
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/game/')
+@app.route('/game/<token>')
+def load_game(token=None):
+    if(token==None):
+        raise WebError('Please enter a valid token')
+    return render_template('game.html', token=token)
